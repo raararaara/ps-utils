e4cce2e15195229104a4ac79a851b81ed544b3e6
임지환 <raararaara@gmail.com>
2024-02-09 01:57:31 +0900

    Update Hash template : std::vector into std::array

--- Utils/Hashing.hpp
+++ Utils/Hashing.hpp
@@ -2,38 +2,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-/** Usage
-* Hashing<p> strH = Hashing<p> (s, modSet);
-**/
+const int msz = 2, globalM = 1000001333;
+array<long long, msz> mod= {1000000009, 1000000007};
 
-vector<long long> modSet= {1000000007, 1000000009, 998244353};
-const long long p = 524287;
-template<long long p>
+template<int p>
 struct Hashing {
-    int N{}, msz{};
-    vector<long long> mod;
+    int N;
     vector<vector<long long>> pw, h;
     Hashing() = default;
-    Hashing(const string &str, vector<long long> &m): N(int(size(str))),msz(int(size(m))),mod(m){
+    Hashing(const string &str): N(int(size(str))) {
         h = pw = vector<vector<long long>>(N, vector<long long>(msz));
 
         for(int j = 0; j < msz; j++) pw[0][j] = 1LL, h[0][j] = str[0];
         for(int i = 1; i < N; i++) {
             for (int j = 0; j < msz; j++) {
-                h[i][j] = (h[i - 1][j] * p + str[i]) % mod[j];
-                pw[i][j] = (pw[i - 1][j] * p) % mod[j];
-                if(h[i][j] < 0) h[i][j] += mod[j];
-                if(pw[i][j] < 0) pw[i][j] += mod[j];
+                h[i][j] = (h[i-1][j] * p + str[i]) % mod[j];
+                pw[i][j] = (pw[i-1][j] * p) % mod[j];
             }
         }
     }
 
-    vector<long long> get(int l, int r) {
-        vector<long long> ret(msz);
+    long long get(int l, int r) {
+        long long ret = 1;
         for(int j = 0; j < msz; j++) {
-            ret[j] = h[r][j];
-            if(l) ret[j] = (ret[j] - h[l-1][j] * pw[r-l+1][j]) % mod[j];
-            if(ret[j] < 0) ret[j] += mod[j];
+            long long x = h[r][j];
+            if(l) x = (x - h[l-1][j] * pw[r-l+1][j]) % mod[j];
+            if(x < 0) x += mod[j];
+
+            ret = (ret * globalM) + x;
         }
         return ret;
     }
